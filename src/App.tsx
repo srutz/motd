c7523bcb190466f98abfecb017d25dfe4056d145
sr <stepan.rutz@gmx.de>
2026-04-30 11:40:16 +0200

    card without anything

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from "react"
 import { Card } from "./Card"
 
 export function App() {
-  const [ expanded, setExpanded ] = useState(false)
   return (
     <div title="App">
-      <Card expanded={expanded}
+      <Card
         title={"Informationsblock"}
         description="Aktuelle Informationen bitte auf Blitzer achten."
         >
           <div>Ich bin rot</div>
       </Card>
-      <button onClick={() => setExpanded(!expanded)}>Toggle</button>
     </div>
   )
 }
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,19 +1,24 @@
-import { type ReactNode } from "react"
+import { useState, type ReactNode } from "react"
 import { LuTriangle } from "react-icons/lu"
 
 export 
-function Card({ expanded, children, title, description}
+function Card({ children, title, description}
     : { children?: ReactNode, expanded?: boolean, 
         title?: string, description?: string}) 
 {
-    const triangleClasses = "mt-1 " + (expanded ? "rotate-180" : "rotate-90")
+    const [ expanded, setExpanded ] = useState(!false)
+    const triangleClasses = (expanded ? "rotate-180" : "rotate-90")
     console.log(triangleClasses)
     return (
         <div className="borderbox">
-            <div className="flex gap-1">{/* layout von links nach rechts */}
-                <LuTriangle size={9} className={triangleClasses}/>
-                <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-1">{/* layout von links nach rechts */}
+                <div className="flex gap-1 items-center">
+                    <button onClick={() => setExpanded(!expanded)} >
+                        <LuTriangle size={9} className={triangleClasses}/>
+                    </button>
                     <div className="text-gray-500 text-xs font-bold">{title}</div>
+                </div>
+                <div className="flex flex-col gap-1 ml-3">
                     {expanded && <div className="text-gray-400 text-xs mb-2">
                         {description}
                     </div>}
