6bb4f1d38f8085ae3dd26c3bc4aa76d0bd2b99ac
sr <stepan.rutz@gmx.de>
2026-04-30 11:08:10 +0200

    card without anything

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react"
 import { Card } from "./Card"
 
 export function App() {
-  console.log("Render App")
+  const [ expanded, setExpanded ] = useState(false)
   return (
     <div title="App">
-      <Card><div>Ich bin rot</div></Card>
+      <Card expanded={expanded}><div>Ich bin rot</div></Card>
+      <button onClick={() => setExpanded(!expanded)}>Toggle</button>
     </div>
   )
 }
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,10 +1,16 @@
 import { type ReactNode } from "react"
+import { FaArrowRight } from "react-icons/fa"
+import { LuAirVent } from "react-icons/lu"
 
 export 
-function Card(props: { children?: ReactNode, expanded?: boolean }) {
+function Card({ expanded, children, title = "Titel 123"}
+    : { children?: ReactNode, expanded?: boolean, title?: string }) 
+{
     return (
         <div className="borderbox">
-            {props.children}
+            {expanded ? <FaArrowRight></FaArrowRight> : <LuAirVent />}
+            <div className="text-gray-500 text-xs">{title}</div>
+            {expanded && children}
         </div>
     )
 }
