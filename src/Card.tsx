eaf542fef410f4288d1695811e5b075239cbe282
sr <stepan.rutz@gmx.de>
2026-04-30 11:52:04 +0200

    card without anything

--- src/Card.tsx
+++ src/Card.tsx
@@ -7,24 +7,40 @@ function Card({ children, title, description}
         title?: string, description?: string}) 
 {
     const [ expanded, setExpanded ] = useState(!false)
-    const triangleClasses = (expanded ? "rotate-180" : "rotate-90")
-    console.log(triangleClasses)
     return (
-        <div className="borderbox">
-            <div className="flex flex-col gap-1">{/* layout von links nach rechts */}
-                <div className="flex gap-1 items-center">
-                    <button onClick={() => setExpanded(!expanded)} >
-                        <LuTriangle size={9} className={triangleClasses}/>
-                    </button>
-                    <div className="text-gray-500 text-xs font-bold">{title}</div>
-                </div>
-                <div className="flex flex-col gap-1 ml-3">
-                    {expanded && <div className="text-gray-400 text-xs mb-2">
-                        {description}
-                    </div>}
-                    {expanded && children}
-                </div>
-            </div>
+    <div className="borderbox">
+        <div className="flex flex-col gap-1">{/* layout von links nach rechts */}
+            <CardHeader expanded={expanded} setExpanded={setExpanded}>
+                {title}
+            </CardHeader>
+            <CardContent>
+                {expanded && <div className="text-gray-400 text-xs mb-2">
+                    {description}
+                </div>}
+                {expanded && children}
+            </CardContent>
         </div>
+    </div>
+    )
+}
+
+function CardContent({children} : { children?: ReactNode}) {
+    return (
+        <div className="flex flex-col gap-1 ml-3">
+            {children}
+        </div>
+    )
+}
+
+function CardHeader({ children, expanded, setExpanded } 
+    : { children?: ReactNode, expanded: boolean, setExpanded: (e: boolean) => void}) {
+    const triangleClasses = (expanded ? "rotate-180" : "rotate-90")
+    return (
+        <div className="flex gap-1 items-center">
+            <button onClick={() => setExpanded(!expanded)} >
+                <LuTriangle size={9} className={triangleClasses}/>
+            </button>
+            <div className="text-gray-500 text-xs font-bold">{children}</div>
+        </div>        
     )
 }
