831cc62d1212b8e11b324c58042f1fdb480f591d
sr <stepan.rutz@gmx.de>
2026-05-04 13:49:35 +0200

    product panel

--- src/useProduct.ts
+++ src/useProduct.ts
@@ -1,7 +1,21 @@
 import { useEffect, useState } from "react";
 import type { Product } from "./types";
+import { useQuery } from "@tanstack/react-query";
 
+// useproduct mit tanstack-query
 export function useProduct(id: number) {
+    return useQuery({
+        queryKey: [ "product", id],
+        queryFn: async () => {
+            const resp = await fetch("https://dummyjson.com/products/" + id)
+            const json = await resp.json()
+            return json as Product;
+        }     
+    })
+}
+
+// product manuell laden
+export function useProduct____(id: number) {
     const [ product, setProduct] = useState<Product|null>(null)
     useEffect(() => {
         (async () => {
